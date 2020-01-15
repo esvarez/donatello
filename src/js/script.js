1b4e2942651523fc04be7d224529c4cff7b774d3
Erick Suarez <ericksuarezdev@gmail.com>
2020-01-14 20:56:07 -0600

    Add task

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,50 @@
+let taskContainer = document.getElementById('task-container');
+
+
+
+function setTask() {
+  //let tasks = data.tasks
+  let tasks = [
+   {id: 1, name: 'One', done: true},
+   {id: 2, name: 'Two Two', done: false},
+   {id: 3, name: 'Three Three Three', done: true},
+   {id: 4, name: 'four four four four', done: true},
+ ]
+  tasks.sort(compare)
+
+  let fragment = document.createDocumentFragment()
+  tasks.forEach(task => {
+    let label = document.createElement('label'),
+        input = document.createElement('input'),
+        spanCheck = document.createElement('span'),
+        spanText = document.createElement('span');
+
+    input.type = 'checkbox'
+    if(task.done){
+      input.checked='checked'
+    }
+    spanCheck.classList.add('checkmark')
+    spanText.innerText = task.name;  
+    label.classList.add('container')      
+    label.appendChild(input)
+    label.appendChild(spanCheck)
+    label.appendChild(spanText)
+    fragment.appendChild(label)
+  });
+  taskContainer.appendChild(fragment)
+}
+
+function compare(a, b) {
+  return (a.done)? 1 : -1;
+}
+
+
 console.log('...')
 const checkbox = document.getElementById("checkbox"),
 btn = document.getElementById("btn"),
 form = document.getElementById("form"),
-btm = document.getElementById("btm")
+btm = document.getElementById("btm"),
+label = document.getElementById("label")
 
 btn.addEventListener("click", function(){       
       //checkbox.checked = true
@@ -24,3 +66,5 @@ label.addEventListener('click', function() {
 /* button.addEventListener("click", function(){ 
    console.log(checkbox)
 }); */
+
+setTask()
