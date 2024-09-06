307f6871113d78f4821fd01f86b4039787b24640
Erick Suarez <ericksuarezdev@gmail.com>
2024-09-05 19:05:36 -0600

    fix main

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
-import "github.com/fogleman/gg"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
+	"time"
 
+	"github.com/esvarez/donatello/sketch"
+)
 
 var (
-	sourceImgName = "source/img1.jpg"
-	outImgName = "out/img.ong"
+	sourceImgName   = "source/img1.jpg"
+	outImgName      = "out/img.ong"
 	totalCycleCount = 500
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
+	rand.NewSource(time.Now().Unix())
 
-
-	img, err := loadImage(sourceImage)
+	img, err := loadImage(sourceImgName)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -21,31 +29,47 @@ func main() {
 	desWith := 2000
 
 	sketch := sketch.NewSketch(img, sketch.UserParams{
-		DesWidth: desWith,
-		DesHeight: 2000,
-		StrokeRatio: 0.75,
-		StrokeReduction: 0.002,
-		StrokeInventionThreshold : 0.05,
-		StrokeJitter: int(0.1 * float64(desWidth)),
-		InitialAlpha: 0.1,
-		AlphaIncrease: 0.36,
-		MinEdgeCount: 3,
-		MaxEdgeCount:4,
+		DesWidth:                 desWith,
+		DesHeight:                2000,
+		StrokeRatio:              0.75,
+		StrokeReduction:          0.002,
+		StrokeInventionThreshold: 0.05,
+		StrokeJitter:             int(0.1 * float64(desWidth)),
+		InitialAlpha:             0.1,
+		AlphaIncrease:            0.36,
+		MinEdgeCount:             3,
+		MaxEdgeCount:             4,
 	})
 
-	for i := 0; i<totalCycleCount; i++ {
+	for i := 0; i < totalCycleCount; i++ {
 		sketch.Update()
 	}
 
-	saveOutput(s.Output(), outImageName)
+	saveOutput(s.Output(), outImgName)
 }
 
-func saveOutput (img image.Image, filePath string) error {
+func loadImage(filePath string) (image.Image, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("source file could not be loaded: %w", err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, fmt.Errorf("source image format could not be decoded: %w", err)
+	}
+
+	return img, nil
+
+}
+
+func saveOutput(img image.Image, filePath string) error {
 	f, err := onCreate(filePath)
 	if err != nil {
-		return err 
+		return err
 	}
-	defer.Close()
+	defer f.Close()
 
 	if err := png.Encode(f, img); err != nil {
 		return nil
--- sketch/sketch.go
+++ sketch/sketch.go
@@ -1,25 +1,31 @@
 package sketch
 
+import (
+	"image"
+
+	"github.com/fogleman/gg"
+)
+
 type UserParams struct {
-	DesWith int
-	DesHeight int
-	StrokeRatio float64
-	StrokeReduction float64
-	StrokeJitter float64
+	DesWidth                 int
+	DesHeight                int
+	StrokeRatio              float64
+	StrokeReduction          float64
+	StrokeJitter             float64
 	StrokeInventionThreshold float64
-	InitialAlpha float64
-	AlphaIncrease float64
-	MinEdgeCount int
-	MaxEdgeCount int
+	InitialAlpha             float64
+	AlphaIncrease            float64
+	MinEdgeCount             int
+	MaxEdgeCount             int
 }
 
 type Sketch struct {
-	UserParams 
-	source image.Image
-	dc *gg.Context
-	sourceWidth int 
-	sourceHeight int
-	strokeSize float64
+	UserParams
+	source            image.Image
+	dc                *gg.Context
+	sourceWidth       int
+	sourceHeight      int
+	strokeSize        float64
 	initialStrokeSize float64
 }
 
@@ -28,15 +34,15 @@ func NewSketch(source image.Image, userParams UserParams) *Sketch {
 	bounds := source.Bounds()
 	s.sourceWidth, s.sourceHeight = bounds.Max.X, bounds.Max.Y
 	initialStrokeSize = s.StrokeRatio * float64(s.DesWidth)
-	s.strokeSize = initialStrokeSize 
+	s.strokeSize = initialStrokeSize
 
 	canvas := gg.NewContext(s.DesWidth, s.DesHeight)
 	canvas.SetColor(color.black)
-	canvas.DrawRectangle(0,0, float64(s.DestWith), float64(s.DestHeight))
+	canvas.DrawRectangle(0, 0, float64(s.DestWith), float64(s.DestHeight))
 	canvas.Fill()
 
-	s.source = source 
-	s.dc = canvas 
+	s.source = source
+	s.dc = canvas
 	return s
 }
 
